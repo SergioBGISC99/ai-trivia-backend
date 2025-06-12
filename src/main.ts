9ab27acdb340803bda504878c14b8239b092b4b2
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-12 00:56:27 -0700

    Adds Docker Compose and configures Swagger
    Sets up a Docker Compose configuration for local development with a PostgreSQL database.
    
    Implements Swagger documentation for the API, enhancing discoverability and ease of use. Configures global validation pipes and CORS.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('MainLogger');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Mentify API')
+    .setDescription('Documentación de la API para la geeración de trivias')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT);
+  logger.log(`Server running on port ${PORT}`);
 }
 bootstrap();
